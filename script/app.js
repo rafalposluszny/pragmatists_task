0f7941304fa91b58bd6eabeae56ef134323cfc5d
RApos <rapos75@gmail.com>
2016-09-10 12:45:59 +0200

    create user list (angularjs)

--- script/app.js
+++ script/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+(function () {
+    var app = angular.module('UserList', []);
+    app.controller('BaseController', BaseControllerCtrl);
+    
+    function BaseControllerCtrl ($scope, $http) {
+        $scope.people = [];
+        
+        $http.get('data/user-data.json')
+            .success(function (response) {
+                $scope.people = response;
+            })
+    }
+}) ();
