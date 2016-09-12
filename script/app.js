29023d8a20c8630d2dbae15fd28dbf4850560f40
RApos <rapos75@gmail.com>
2016-09-12 10:41:34 +0200

    add "edit" function

--- script/app.js
+++ script/app.js
@@ -5,27 +5,39 @@
     app.controller('BaseController', BaseControllerCtrl);
     
     function BaseControllerCtrl ($scope, $http) {
+
         $scope.people = [];
-        $scope.newName = '';
-        $scope.newEmail = '';
-        
+
         $http.get('data/user-data.json')
             .then(function successCallback(response) {
                 $scope.people = response.data;
+
             }, function errorCallback(response) {
                 $scope.status = response.status;
                 $scope.statusText = response.statusText;
                 $('#modalError').modal('show');
             });
 
-        $scope.modalAction = function () {
-            $scope.newName = '';
-            $scope.newEmail = '';
-            $('#modalAction').modal('show');
+        $scope.modalCreate = function () {
+            $scope.newUser = null;
+            $('#modalCreate').modal('show');
         };
 
         $scope.addUser = function () {
-            $scope.people.push({name: $scope.newName, email: $scope.newEmail});
+            $scope.people.push($scope.newUser);
+            $('#modalCreate').modal('hide');
+            console.log($scope.newUser)
+        };
+
+        $scope.modalEdit = function (person) {
+            $scope.editedUser = angular.copy(person);
+            $scope.id = $scope.people.indexOf(person);
+            $('#modalEdit').modal('show');
+        };
+
+        $scope.saveUser = function () {
+            $scope.people[$scope.id] = $scope.editedUser;
+            $('#modalEdit').modal('hide');
         };
 
         $scope.removeUser = function (person) {
